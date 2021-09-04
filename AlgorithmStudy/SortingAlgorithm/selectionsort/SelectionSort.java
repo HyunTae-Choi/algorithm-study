60e9eb513d254f22cb9b72adc42c4c9b3112ca80
HyunTaeChoi <chsachd1231@gmail.com>
2021-09-05 01:42:10 +0900

    선택정렬 알고리즘 - 주석추가 2021.09.05

--- AlgorithmStudy/SortingAlgorithm/selectionsort/SelectionSort.java
+++ AlgorithmStudy/SortingAlgorithm/selectionsort/SelectionSort.java
@@ -7,6 +7,12 @@ import java.util.Arrays;
  * @version 1.0 2021.09.04
  * Sort (정렬) : 일정한 순서대로 열거하는 알고리즘
  * SelectionSort (선택정렬) : 현재 위치에 들어갈 데이터를 찾아 선택하여 정렬하는 알고리즘
+ * 장점
+ * 1. 구현이 쉽다.
+ * 2. 추가적인 메모리 소비가 작다.
+ * 단점
+ * 1. 시간이 오래걸리는 정렬방식이다.
+ * 2. 안정 정렬이 아니다.
  * */
 public class SelectionSort {
 	public static void main(String[] args) {
