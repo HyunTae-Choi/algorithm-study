2c46245bd9a1b5e9cc4fab465820193f69006103
HyunTaeChoi <chsachd1231@gmail.com>
2021-09-04 17:43:47 +0900

    이분탐색 알고리즘 - 소스, 주석 추가 2021.09.04

--- AlgorithmStudy/src/binarysearch/BinarySearch.java
+++ AlgorithmStudy/src/binarysearch/BinarySearch.java
@@ -3,12 +3,18 @@ package binarysearch;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * @author HyeonTaeChoi
+ * @version 1.0 2021.09.04
+ * Search (탐색) : 저장된 정보들 중에 원하는 값을 찾는 것
+ * BinarySearch(이분탐색) : 정렬되어 있는(이분 탐색의 주요 조건) 배열에서 데이터를 찾으려 시도할 때, 탐색 범위를 절반씩 줄여가며 찾아가는 Search 방법이다.
+ * */
 public class BinarySearch {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		int[] arr = { 3, 7, -1, 4, 11, 0, 7, 9 }; // 배열
+		int[] arr = { 3, 8, -1, 4, 11, 0, 7, 9 }; // 배열
 
 		System.out.println("배열 : " + Arrays.toString(arr));
 
@@ -46,25 +52,35 @@ public class BinarySearch {
 
 	// 이분탐색 메소드
 	public static int Search(int[] arr, int find) {
-		int left = 0; // 첫번째 숫자
-		int mid = 0; // 가운데 숫자
-		int right = arr.length - 1; // 마지막 숫자
+		int min = 0; // 배열 중 시작 번째
+		int mid = 0; // 전체 배열 중 가운데
+		int max = arr.length-1; // 배열 중 마지막 번째
+		while(min < max) {
+			if((min + max) % 2 == 1) {
+				mid = ((min + max) / 2) + 1;
+			} else {
+				mid = (min + max) / 2;
+			}
+			
+			if(find == arr[mid]) {
+				return mid + 1;
+			} else if(find > arr[mid]) {
+				min = mid + 1;
+				if(min == max) {
+					if(find == arr[max]) {
+						return max + 1;
+					}
+				}
+			} else {
+				max = mid - 1;
+				if(max == min) {
+					if(find == arr[min]) {
+						return min + 1;
+					}
+				}
+			}
+		}
 		
-		while (right >= left) {
-            mid = (right + left) / 2;
- 
-            if (find == arr[mid]) {
-                return mid;
-            }
- 
-            if (find < arr[mid]) {
-                right = mid - 1;
-                return right;
-            } else {
-                left = mid + 1;
-                return left;
-            }
-        }
 		return -1;
 	}
 }
