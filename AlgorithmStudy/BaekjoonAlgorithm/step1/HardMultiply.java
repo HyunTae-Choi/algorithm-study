4d8abe42033f5b205a754042a0e77694662ed763
HyunTaeChoi <chsachd1231@gmail.com>
2021-09-05 23:22:19 +0900

    백준 알고리즘 Step 1 - 2021.09.05

---
+++ AlgorithmStudy/BaekjoonAlgorithm/step1/HardMultiply.java
@@ -0,0 +1,26 @@
+package step1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HardMultiply {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		
+		int x = (b % 100) % 10;
+		int y = (b % 100) / 10;
+		int z = b / 100;
+		
+		System.out.println(a * x);
+		System.out.println(a * y);
+		System.out.println(a * z);
+		System.out.println(a * b);
+		
+	}
+
+}
