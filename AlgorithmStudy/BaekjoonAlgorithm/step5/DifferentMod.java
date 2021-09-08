803dfa14a3ef755c5179e63f442d7164899cc755
HyunTaeChoi <chsachd1231@gmail.com>
2021-09-08 21:03:59 +0900

    백준 알고리즘 Step 5 - 2021.09.08

--- AlgorithmStudy/BaekjoonAlgorithm/step5/DifferentMod.java
+++ AlgorithmStudy/BaekjoonAlgorithm/step5/DifferentMod.java
@@ -10,24 +10,21 @@ public class DifferentMod {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int[] modArr = new int[10];
-		int count = 0;
+		boolean[] modOverlap = new boolean[42];
 		int result = 0;
 		
 		for(int i = 0; i < 10; i++) {
 			modArr[i] = Integer.parseInt(br.readLine()) % 42;
+			modOverlap[modArr[i]] = true;
+		}
+		for(boolean count : modOverlap) {
+			if(count) {
+				result++;
+			} else {
+				continue;
+			}
 		}
 		
-		for(int i = 0; i < 10; i++) {
-			count = 0;
-			for(int j = i + 1; j < 10; j++) {
-				if(modArr[i] == modArr[j]) {
-					count++;
-				}
-			}
-			if(count == 0) {
-				result++;
-			}
-		}
 		System.out.println(result);
 	}
 
