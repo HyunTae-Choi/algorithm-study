031dcca1da14edcafb14af61ba219ed7dba03dd9
HyunTaeChoi <chsachd1231@gmail.com>
2021-09-09 23:50:42 +0900

    백준 알고리즘 Step 7 - 2021.09.09

---
+++ AlgorithmStudy/BaekjoonAlgorithm/step7/CroatiaAlphabet.java
@@ -0,0 +1,68 @@
+package step7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CroatiaAlphabet {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		int alphabetCount = 0;
+		for(int i = 0; i < str.length(); i++) {
+			char alphabet = str.charAt(i);
+			if(alphabet == 'c') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == '=') {
+						i++;
+					} else if(str.charAt(i + 1) == '-') {
+						i++;
+					}
+				}
+			} else if(alphabet == 'd') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == 'z') {
+						if(i < str.length() - 2) {
+							if(str.charAt(i + 2) == '=') {
+								i += 2;
+							}
+						}
+					} else if(str.charAt(i + 1) == '-') {
+						i++;
+					}
+				}
+			} else if(alphabet == 'l') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == 'j') {
+						i++;
+					}
+				}
+			} else if(alphabet == 'n') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == 'j') {
+						i++;
+					}
+				}
+			} else if(alphabet == 's') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == '=') {
+						i++;
+					}
+				}
+			} else if(alphabet == 'z') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == '=') {
+						i++;
+					}
+				}
+			} 
+			alphabetCount++;
+		}
+		
+		System.out.println(alphabetCount);
+	}
+
+}
