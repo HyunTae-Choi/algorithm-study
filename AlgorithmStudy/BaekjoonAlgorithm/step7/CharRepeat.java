7924cb577dace36e971d2bcdc55cdfcc8efb4629
HyunTaeChoi <chsachd1231@gmail.com>
2021-09-09 22:35:22 +0900

    백준 알고리즘 Step 7 - 2021.09.09

---
+++ AlgorithmStudy/BaekjoonAlgorithm/step7/CharRepeat.java
@@ -0,0 +1,49 @@
+package step7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CharRepeat {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int repeat = Integer.parseInt(br.readLine());
+		String[] testCase = new String[repeat];
+		
+		StringTokenizer st;
+		int charRepeat = 0;
+		String charStr = "";
+		
+		for(int i = 0; i < repeat; i++) {
+			testCase[i] = br.readLine();
+		}
+		
+		for(int i = 0; i < repeat; i++) {
+			st = new StringTokenizer(testCase[i], " ");
+			charRepeat = Integer.parseInt(st.nextToken());
+			charStr = st.nextToken();
+			repeat(charRepeat, charStr);
+		}
+		
+	}
+
+	private static void repeat(int charRepeat, String charStr) {
+		char[] charArr = new char[charStr.length() * charRepeat];
+		int arrLocation = 0;
+		int count = 0;
+		for(int i = 0; i < charStr.length(); i++) {
+			while(count < charRepeat) {
+				charArr[arrLocation] = charStr.charAt(i);
+				count++;
+				arrLocation++;
+			}
+			count = 0;
+		}
+		
+		System.out.println(charArr);
+	}
+
+}
---
+++ AlgorithmStudy/BaekjoonAlgorithm/step7/ManyUseAlphabet.java
@@ -0,0 +1,38 @@
+package step7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ManyUseAlphabet {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int[] alphaCount = new int[26];
+		
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) >= 65 && str.charAt(i) <= 90) {
+				alphaCount[str.charAt(i) - 65]++;
+			} else {
+				alphaCount[str.charAt(i) - 97]++;
+			}
+		}
+		
+		int max = 0;
+		char ch = '?';
+		for(int i = 0; i < alphaCount.length; i++) {
+			if(alphaCount[i] > max) {
+				max = alphaCount[i];
+				ch = (char)(65 + i);
+			}
+			else if(alphaCount[i] == max) {
+				ch = '?';
+			}
+		}
+		
+		System.out.println(ch);
+		
+	}
+
+}
