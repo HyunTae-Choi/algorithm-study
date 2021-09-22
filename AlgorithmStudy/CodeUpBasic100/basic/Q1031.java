5a37a19b4c42f4e8e3ffe442be54f8cfa2d7d8de
HyunTaeChoi <chsachd1231@gmail.com>
2021-09-22 16:46:46 +0900

    코드업 기초 100제 자바로 풀기 - 2021.09.22

---
+++ AlgorithmStudy/CodeUpBasic100/basic/Q1031.java
@@ -0,0 +1,18 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q1031 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int x = Integer.parseInt(br.readLine());
+		String result = Integer.toOctalString(x);
+		
+		System.out.println(result);
+	}
+	
+}
---
+++ AlgorithmStudy/CodeUpBasic100/basic/Q1039.java
@@ -0,0 +1,21 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1039 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		
+		System.out.println(a + b);
+		
+	}
+	
+}
